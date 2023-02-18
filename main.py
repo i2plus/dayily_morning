0c3f0adb44dd7f3f2174a0dcb81870d5d17a15fb
i2plus <49112208+i2plus@users.noreply.github.com>
2023-02-18 23:59:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def get_weather():
   url = "https://v0.yiketianqi.com/api?unescape=1&version=v91&appid=43656176&appsecret=I42og6Lm&ext=&cityid=&city=" + city
   res = requests.get(url).json()
   weather = res['data'][0]
-  return weather['wea'], math.floor(weather['tem']), math.floor(weather['tem1']), math.floor(weather['tem2'])
+  return weather['wea'], math.floor(int(weather['tem'])), math.floor(int(weather['tem1'])), math.floor(int(weather['tem2']))
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
