278603df8d7b9d8f49a46dfc3b6c0df3777b4611
i2plus <49112208+i2plus@users.noreply.github.com>
2023-02-18 23:21:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ template_id = os.environ["TEMPLATE_ID"]
 
 
 def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+  url = "https://v0.yiketianqi.com/api?unescape=1&version=v91&appid=43656176&appsecret=I42og6Lm&ext=&cityid=&city=" + city
   res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp']), math.floor(weather['high']), math.floor(weather['low'])
+  weather = res['data'][0]
+  return weather['wea'], math.floor(weather['tem']), math.floor(weather['tem1']), math.floor(weather['tem2'])
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
